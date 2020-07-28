b4931e76b97f890842d6b2a007c5c47c024482f5
roncwynn <rcwynn@gmail.com>
2020-07-28 11:03:05 -0400

    WIP Working on models and Context

---
+++ backend-api/RopeFinalProjectBackEnd/Contexts/ReleaseTasksAPIContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RopeFinalProjectBackEnd.Models;
+
+
+namespace RopeFinalProjectBackEnd.Contexts
+{
+    public class ReleaseTasksAPIContext : DbContext
+    {
+        public DbSet<ReleaseTask> ReleaseTasks { get; set; }
+        public DbSet<Employee> Peoples { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<Priority> Priorities { get; set; }
+    }
+}
---
+++ backend-api/RopeFinalProjectBackEnd/Models/ReleaseTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RopeFinalProjectBackEnd.Models
+{
+    public class ReleaseTask
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CurrentStatusID { get; set; }
+        public int CurrentPriorityID { get; set; }
+        public DateTime CurrentDueTime { get; set; }
+        public bool IsVisisble { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public int AssignedEmployeeID { get; set; }
+    }
+}
