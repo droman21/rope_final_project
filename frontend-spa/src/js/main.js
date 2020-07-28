fb3a94e5576b511814c16d467df4fb496b581540
Bernard828 <bernard.lackey1@gmail.com>
2020-07-28 13:03:47 -0400

    added Employees, component, getAll added to main.js

--- frontend-spa/src/js/main.js
+++ frontend-spa/src/js/main.js
@@ -12,12 +12,38 @@ export default function pagebuild(){
     header()
     footer()
     home()
-    showReleaseTask()
-    showEmployees()
+    showReleaseTasks()
+    showEmployeeList()
     showStatus()
-    showPririty()    
+    showPriority()    
 }
-function header() {
-    const header = document.querySelector('.header');
-    header.innerHTML = Header();
+function showReleaseTasks() {
+    fetch()
+        .then(response => response.json())
+        .then(releaseTasks => {
+            appDiv.innerHTML = ReleaseTasks(releaseTasks);
+        })
+        .catch(err => console.log(err))
+}
 
+function showEmployeeList() {
+    fetch()
+        .then(response => response.json())
+        .then(employees => {
+            appDiv.innerHTML = Employees(employees);
+        })
+        .catch(err => console.log(err))
+}
+function showReleaseTask_ByPriority() {
+    const priorityReleaseTask = document.querySelectorAll(".releaseTask__item");
+    priorityReleaseTask.forEach(element => {
+        element.addEventListener('click', function () {
+            const priorityReleaseTaskId = element.id;
+            fetch()
+                .then(response => response.json())
+                .then( => appDiv.innerHTML = )
+                .catch(err => console.log(err))
+        })
+    })
+
+}
