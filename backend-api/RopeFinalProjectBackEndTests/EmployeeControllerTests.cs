346ec0ab6448dcfaf674b80897b42c8c4ea0e543
Dakota <Dakotawhittle1203@gmail.com>
2020-07-29 14:27:00 -0400

    added priority and status controller tests

---
+++ backend-api/RopeFinalProjectBackEndTests/EmployeeControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using NSubstitute;
+using RopeFinalProjectBackEnd.Models;
+using RopeFinalProjectBackEnd.Repositories;
+using RopeFinalProjectBackEnd.Controllers;
+
+namespace RopeFinalProjectBackEndTests
+{
+    public class EmployeeControllerTests
+    {
+        EmployeeController underTest;
+        IRepository<Employee> employeeRepo;
+
+        public EmployeeControllerTests()
+        {
+            employeeRepo = Substitute.For<IRepository<Employee>>();
+            underTest = new EmployeeController(employeeRepo);
+        }
+        [Fact]
+        public void Get_By_Id_Returns_Valid_Release_Task()
+        {
+            var newRelease = new Employee();
+            employeeRepo.GetById(1).Returns(newRelease);
+            var result = underTest.Get(1);
+            Assert.Equal(newRelease, result);
+        }
+    }
+}
---
+++ backend-api/RopeFinalProjectBackEndTests/PriorityControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using NSubstitute;
+using RopeFinalProjectBackEnd.Models;
+using RopeFinalProjectBackEnd.Controllers;
+using RopeFinalProjectBackEnd.Repositories;
+
+namespace RopeFinalProjectBackEndTests
+{
+    public class PriorityControllerTests
+    {
+        PriorityController underTest;
+        IRepository<Priority> priorityRepo;
+
+        public PriorityControllerTests()
+        {
+            priorityRepo = Substitute.For<IRepository<Priority>>();
+            underTest = new PriorityController(priorityRepo);
+        }
+        [Fact]
+        public void Get_By_Id_Returns_Valid_Release_Task()
+        {
+            var newRelease = new Priority();
+            priorityRepo.GetById(1).Returns(newRelease);
+            var result = underTest.Get(1);
+            Assert.Equal(newRelease, result);
+        }
+    }
+}
