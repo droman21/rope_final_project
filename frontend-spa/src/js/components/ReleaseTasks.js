745993d48d9c9a1f932a3aa06182de1f23e979b2
roncwynn <rcwynn@gmail.com>
2020-07-29 20:07:42 -0400

    WIP added table to main page

--- frontend-spa/src/js/components/ReleaseTasks.js
+++ frontend-spa/src/js/components/ReleaseTasks.js
@@ -1,17 +1,33 @@
 export default function ReleaseTasks(releaseTasks) {
     return `
         <h1><u>Release Tasks123</h1></u>
-            ${releaseTasks.map(releaseTask => {
+        <section>
+        <div class="container">
+            <div class="floatLeft">
+                <table class="table-1" style="width:100%; float:left">        
+                    <tr class="table_header">
+                        <th class="table_headerFont">ID</th>
+                        <th>Task Name</th>
+                        <th>Status</th>
+                        <th>Priority</th>
+                        <th>DueTime</th>
+                        <th>Assigned To</th>
+                    </tr>
+        ${releaseTasks.map(releaseTask => {
         return `
-            <div id="releaseTask">
-                <ul>
-                <article>
-                    <a href="#" class="releaseTask__info" id="${releaseTask.id}">${releaseTask.name}</a>
-                </article>
-                </ul>
-            </div>
-            
+            <tr class="table1__rowFont">
+                <td>${releaseTask.id}</td>
+                <td><a href="#" class="releaseTask__info" id="${releaseTask.id}">${releaseTask.name}</a></td>
+                <td>${releaseTask.currentStatusID}</td>
+                <td>${releaseTask.currentPriorityID}</td>
+                <td>${releaseTask.currentDueTime}</td>
+                <td>${releaseTask.assignedEmployeeID}</td>
+            </tr>
                 `
-    }).join("")}
+            }).join("")}
+                </table>
+            </div>    
+        </div>
+        </section>
     `
 }
