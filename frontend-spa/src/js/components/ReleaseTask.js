3856f62577bccfc6e98f7fac165d8d0345e4dc4a
Dakota <Dakotawhittle1203@gmail.com>
2020-07-31 11:22:59 -0400

    Added text styling to releasetask.js, rearranged fields

--- frontend-spa/src/js/components/ReleaseTask.js
+++ frontend-spa/src/js/components/ReleaseTask.js
@@ -4,15 +4,31 @@ export default function ReleaseTask(releaseTask) {
     return `
     <h1 class="table-2__header"><a href="#" class="edit__releaseTask" id="${releaseTask.id}">${releaseTask.name}</a></h1>
     <ul class="releaseTask__info">
-        <li class="releaseTask__id" >ID: ${releaseTask.id}</li>
-        <li class="releaseTask__name" id="${releaseTask.id}">NAME: ${releaseTask.name}</li>
-        <li class="releaseTask__description">DESCRIPTION: ${releaseTask.description}</li>
-        <li class="releaseTask__currentStatusId">STATUS-ID ${releaseTask.currentStatusID}</li>
-        <li class="releaseTask__currentPriorityId">PRIORITY-ID ${releaseTask.currentPriorityID}</li>
-        <li class="releaseTask__currentDueTime">DUE-TIME: ${releaseTask.currentDueTime}</li>
-        <li class="releaseTask__createdDate">CREATED: ${releaseTask.createdDate}</li>
-        <li class="releaseTask__lastModifiedDate">LAST-MODIFIED: ${releaseTask.lastModifiedDate}</li>
-        <li class="releaseTask__assignedEmployeeId">ASSIGNED-TO: ${releaseTask.assignedEmployeeID}</li>
+        <li class="releaseTask__currentDueTime"><u><b>DUE-TIME:</b></u> ${releaseTask.currentDueTime}</li>
+        <br>
+        <br>
+        <li class="releaseTask__id" ><u><b>ID:</b></u> ${releaseTask.id}</li>
+        <br>
+        <br>
+        <li class="releaseTask__name" id="${releaseTask.id}"><u><b>NAME:</b></u> ${releaseTask.name}</li>
+        <br>
+        <br>
+        <li class="releaseTask__description"><u><b>DESCRIPTION:</b></u> ${releaseTask.description}</li>
+        <br>
+        <br>
+        <li class="releaseTask__currentStatusId"><u><b>STATUS-ID:</b></u> ${releaseTask.currentStatusID}</li>   
+        <br>
+        <br>
+        <li class="releaseTask__currentPriorityId"><u><b>PRIORITY-ID:</b></u> ${releaseTask.currentPriorityID}</li>
+        <br>
+        <br>
+        <li class="releaseTask__createdDate"><u><b>CREATED:</b></u> ${releaseTask.createdDate}</li>
+        <br>
+        <br>
+        <li class="releaseTask__lastModifiedDate"><u><b>LAST-MODIFIED:</b></u> ${releaseTask.lastModifiedDate}</li>
+        <br>
+        <br>
+        <li class="releaseTask__assignedEmployeeId"><u><b>ASSIGNED-TO:</b></u> ${releaseTask.assignedEmployeeID}</li>
     </ul>
 <button class="edit__releaseTaskButton" id="${releaseTask.id}">Edit</button>
 
