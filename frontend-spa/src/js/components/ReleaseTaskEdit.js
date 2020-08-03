eabefa92088fb213a6df7edac2c6d623e12d828d
Daniel Roman <dan.roman21@gmail.com>
2020-08-03 11:40:25 -0400

    Edit Page: Added field headers, styled save changes, edit, and new task buttons

--- frontend-spa/src/js/components/ReleaseTaskEdit.js
+++ frontend-spa/src/js/components/ReleaseTaskEdit.js
@@ -1,13 +1,21 @@
 //import Employee from "./Employee";
 export default function ReleaseTaskEdit(releaseTask) {
     return `
-        <input class="edit-releaseTask__id" value="${releaseTask.id}">
-        <input class="edit-releaseTask__name" type="text" value="${releaseTask.name}">
-        <input class="edit-releaseTask__description" type="text" value="${releaseTask.description}">
-        <input class="edit-releaseTask__currentStatus" value="${releaseTask.status.name}">
-        <input class="edit-releaseTask__currentPriority" value="${releaseTask.priority.name}">
-        <input class="edit-releaseTask__currentDueTime" value="${releaseTask.currentDueTime}">
-        <input class="edit-releaseTask__assignedEmployeed" value="${releaseTask.employee.name}">
-        <button class="edit-releaseTask__submit"> Save Your Changes </button>
+        <h1>Edit the release task fields below.</h1>
+        <h3>Task ID:
+        <input class="edit-releaseTask__id" value="${releaseTask.id}"></h3>
+        <h3>Task Name:
+        <input class="edit-releaseTask__name" type="text" value="${releaseTask.name}"></h3>
+        <h3>Description of Task:
+        <input class="edit-releaseTask__description" type="text" value="${releaseTask.description}"></h3>
+        <h3>Current Task Status:
+        <input class="edit-releaseTask__currentStatus" value="${releaseTask.status.name}"></h3>
+        <h3>Priority:
+        <input class="edit-releaseTask__currentPriority" value="${releaseTask.priority.name}"></h3>
+        <h3>Due Date: 
+        <input class="edit-releaseTask__currentDueTime" value="${releaseTask.currentDueTime}"></h3>
+        <h3>Assigned Employee:
+        <input class="edit-releaseTask__assignedEmployeed" value="${releaseTask.employee.name}"></h3><br><br>
+        <button class="edit-releaseTask__submit">Save Your Changes</button>
     `
 }
