47f6aa1befc308cfc2e47323d77881ee267edd82
Daniel Roman <dan.roman21@gmail.com>
2020-08-03 14:22:44 -0400

    Changed order of edit fields in Edit task page

--- frontend-spa/src/js/components/ReleaseTaskEdit.js
+++ frontend-spa/src/js/components/ReleaseTaskEdit.js
@@ -2,7 +2,9 @@
 export default function ReleaseTaskEdit(releaseTask) {
     return `
         <h1>Edit the release task fields below.</h1>
-        <h4>Task ID:
+        <h4>Due: 
+        <input class="edit-releaseTask__currentDueTime" value="${releaseTask.currentDueTime}"></h4>
+        <h4>ID:
         <input class="edit-releaseTask__id" value="${releaseTask.id}"></h4>
         <h4>Task Name:
         <input class="edit-releaseTask__name" type="text" value="${releaseTask.name}"></h4>
@@ -12,9 +14,7 @@ export default function ReleaseTaskEdit(releaseTask) {
         <input class="edit-releaseTask__currentStatus" type="dropdown" value="${releaseTask.status.name}"></h4>
         <h4>Priority:
         <input class="edit-releaseTask__currentPriority" value="${releaseTask.priority.name}"></h4>
-        <h4>Due: 
-        <input class="edit-releaseTask__currentDueTime" value="${releaseTask.currentDueTime}"></h4>
-        <h4>Assigned Employee:
+        <h4>Assigned To:
         <input class="edit-releaseTask__assignedEmployeed" value="${releaseTask.employee.name}"></h4><br><br>
         <div class="edit-releaseTask__submitContainer">
         <div class="edit-releaseTask_submitCenter">
