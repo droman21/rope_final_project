46bcbda5312fa642b21f7fafee3c16f9b167d1b9
Daniel Roman <dan.roman21@gmail.com>
2020-08-03 13:34:16 -0400

    Applied updated styling to table 2 and table 2 edit pages

--- frontend-spa/src/js/components/ReleaseTask.js
+++ frontend-spa/src/js/components/ReleaseTask.js
@@ -3,23 +3,23 @@ export default function ReleaseTask(releaseTask) {
     <h1 class="table-2__header"  id="${releaseTask.id}">${releaseTask.name}</h1>
     <section class="releaseTask__detailsInfo">
         <ul class="releaseTask__detailsInfolist">
-            <li class="releaseTask__currentDueTime"><b>DUE-TIME:</b> ${releaseTask.currentDueTime}</li>
+            <li class="releaseTask__currentDueTime"><b>Due:</b> ${releaseTask.currentDueTime}</li>
             <br>
             <li class="releaseTask__id" ><b>ID:</b> ${releaseTask.id}</li>
             <br>
-            <li class="releaseTask__name" id="${releaseTask.id}"><b>NAME:</b> ${releaseTask.name}</li>
+            <li class="releaseTask__name" id="${releaseTask.id}"><b>Task Name:</b> ${releaseTask.name}</li>
             <br>
-            <li class="releaseTask__description"><b>DESCRIPTION:</b> ${releaseTask.description}</li>
+            <li class="releaseTask__description"><b>Description:</b> ${releaseTask.description}</li>
             <br>
-            <li class="releaseTask__currentStatusName"><b>STATUS-NAME:</b> ${releaseTask.status.name}</li>   
+            <li class="releaseTask__currentStatusName"><b>Status:</b> ${releaseTask.status.name}</li>   
             <br>
-            <li class="releaseTask__currentPriorityName"><b>PRIORITY-NAME:</b> ${releaseTask.priority.name}</li>
+            <li class="releaseTask__currentPriorityName"><b>Priority:</b> ${releaseTask.priority.name}</li>
             <br>
-            <li class="releaseTask__createdDate"><b>CREATED:</b> ${releaseTask.createdDate}</li>
+            <li class="releaseTask__createdDate"><b>Created:</b> ${releaseTask.createdDate}</li>
             <br>
-            <li class="releaseTask__lastModifiedDate"><b>LAST-MODIFIED:</b> ${releaseTask.lastModifiedDate}</li>
+            <li class="releaseTask__lastModifiedDate"><b>Last Modified:</b> ${releaseTask.lastModifiedDate}</li>
             <br>
-            <li class="releaseTask__assignedEmployeeName"><b>ASSIGNED-TO:</b> ${releaseTask.employee.name}</li>
+            <li class="releaseTask__assignedEmployeeName"><b>Assigned To:</b> ${releaseTask.employee.name}</li>
             <br>
         </ul>
         <div class="edit-button">
--- frontend-spa/src/js/components/ReleaseTaskEdit.js
+++ frontend-spa/src/js/components/ReleaseTaskEdit.js
@@ -2,20 +2,20 @@
 export default function ReleaseTaskEdit(releaseTask) {
     return `
         <h1>Edit the release task fields below.</h1>
-        <h3>Task ID:
-        <input class="edit-releaseTask__id" value="${releaseTask.id}"></h3>
-        <h3>Task Name:
-        <input class="edit-releaseTask__name" type="text" value="${releaseTask.name}"></h3>
-        <h3>Description of Task:
-        <input class="edit-releaseTask__description" type="text" value="${releaseTask.description}"></h3>
-        <h3>Current Task Status:
-        <input class="edit-releaseTask__currentStatus" value="${releaseTask.status.name}"></h3>
-        <h3>Priority:
-        <input class="edit-releaseTask__currentPriority" value="${releaseTask.priority.name}"></h3>
-        <h3>Due Time: 
-        <input class="edit-releaseTask__currentDueTime" value="${releaseTask.currentDueTime}"></h3>
-        <h3>Assigned Employee:
-        <input class="edit-releaseTask__assignedEmployeed" value="${releaseTask.employee.name}"></h3><br><br>
+        <h4>Task ID:
+        <input class="edit-releaseTask__id" value="${releaseTask.id}"></h4>
+        <h4>Task Name:
+        <input class="edit-releaseTask__name" type="text" value="${releaseTask.name}"></h4>
+        <h4>Description:
+        <input class="edit-releaseTask__description" type="text" value="${releaseTask.description}"></h4>
+        <h4>Current Task Status:
+        <input class="edit-releaseTask__currentStatus" value="${releaseTask.status.name}"></h4>
+        <h4>Priority:
+        <input class="edit-releaseTask__currentPriority" value="${releaseTask.priority.name}"></h4>
+        <h4>Due: 
+        <input class="edit-releaseTask__currentDueTime" value="${releaseTask.currentDueTime}"></h4>
+        <h4>Assigned Employee:
+        <input class="edit-releaseTask__assignedEmployeed" value="${releaseTask.employee.name}"></h4><br><br>
         <div class="edit-releaseTask__submitContainer">
         <div class="edit-releaseTask_submitCenter">
         <button class="edit-releaseTask__submit">Save Your Changes</button></div></div>
