ce4861a8f21655cf7cc39096b6c59f408411912b
Daniel Roman <dan.roman21@gmail.com>
2020-08-06 11:21:35 -0400

    Connected About Us page to nav header link

--- frontend-spa/src/js/components/Header.js
+++ frontend-spa/src/js/components/Header.js
@@ -1,7 +1,8 @@
 export default function Header(_){
     return `
-     <nav class="navbar" 
+     <nav class="nav" 
         <h1 class="header">R.O.P.E. Application</h1>
+        <a href="http://127.0.0.1:5500/docs/about_us.html" target="_blank">About Us</a>
      </nav>
 
     `
