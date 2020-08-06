b8a253ae1b9bd908a64a0d9461183b07ba37b3b9
Daniel Roman <dan.roman21@gmail.com>
2020-08-06 11:35:20 -0400

    Applied initial styling to nav header

--- frontend-spa/src/js/components/Header.js
+++ frontend-spa/src/js/components/Header.js
@@ -1,7 +1,7 @@
 export default function Header(_){
     return `
-     <nav class="nav" 
-        <h1 class="header">R.O.P.E. Application</h1>
+     <nav class="nav"
+     <h1></h1>
         <a href="http://127.0.0.1:5500/docs/about_us.html" target="_blank">About Us</a>
      </nav>
 
