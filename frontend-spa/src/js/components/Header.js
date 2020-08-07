3e7562bc97eb9df025d23e09fbfe4732f466dc9d
Daniel Roman <dan.roman21@gmail.com>
2020-08-07 09:31:36 -0400

    I believe I figured out how to link the nav elements for anyone viewing the app

--- frontend-spa/src/js/components/Header.js
+++ frontend-spa/src/js/components/Header.js
@@ -2,8 +2,8 @@ export default function Header(_){
     return `
      <nav class="nav"
      <h1></h1>
-        <a href="http://127.0.0.1:5500/docs/about_us.html" target="_blank">About Us</a>
-        <a href="http://127.0.0.1:5500/docs/AboutApp.html">About Our App</a>
+        <a href="about_us.html" target="_blank">About Us</a>
+        <a href="AboutApp.html" target="_blank">About Our App</a>
      </nav>
 
     `
