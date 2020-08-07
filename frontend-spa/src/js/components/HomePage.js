62a30f0e83634efe766248e34539c1aed497f427
Daniel Roman <dan.roman21@gmail.com>
2020-08-07 13:29:59 -0400

    Applied styling to home page

--- frontend-spa/src/js/components/HomePage.js
+++ frontend-spa/src/js/components/HomePage.js
@@ -1,8 +1,8 @@
 export default function HomePage() {
     return `
     <h1>R.O.P.E. Application</h1>
-    <h2>Release Tasks</h2>
+    <h2>Your Release: Easily Managed</h2>
     <br>
-        <button class="startapp"><b>Show Active Tasks</b></a></button> 
+        <button class="startapp"><b>Click Here to Begin</b></a></button> 
     `
 }
