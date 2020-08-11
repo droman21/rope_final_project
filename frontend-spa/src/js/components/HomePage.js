ec817a1d2fee97dcc4aba26c9d7ea6b1d6e4e44e
roncwynn <rcwynn@gmail.com>
2020-08-11 14:21:18 -0400

    HomePage Edits added

--- frontend-spa/src/js/components/HomePage.js
+++
@@ -1,7 +0,0 @@
-export default function HomePage() {
-    return `
-    <h2 class="homepageHeader">Your Release: Easily Managed</h2>
-    <br>
-        <button class="startapp"><b>Click Here to Begin</b></a></button> 
-    `
-}
