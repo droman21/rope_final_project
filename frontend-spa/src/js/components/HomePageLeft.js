f71aa3074c48a4c73be024eb0ab27b89412f9f18
Dakota <Dakotawhittle1203@gmail.com>
2020-08-11 13:30:31 -0400

    moved app info into right table on splash screen, added component in main

--- frontend-spa/src/js/components/HomePageLeft.js
+++ frontend-spa/src/js/components/HomePageLeft.js
@@ -1,7 +1,14 @@
-export default function HomePage() {
+export default function HomePageLeft() {
     return `
     <h1>R.O.P.E. Application</h1>
+    <br>
+    <h2>Release <br>
+    Operations <br>
+    Process <br>
+    Enforcer</h2>
+    <br>
     <h2>Your Release: Easily Managed</h2>
+
     <br>
         <button class="startapp"><b>Click Here to Begin</b></a></button> 
     `
--- frontend-spa/src/js/main.js
+++ frontend-spa/src/js/main.js
@@ -14,7 +14,8 @@ import ReleaseTaskPostSection from './components/ReleaseTaskPostSection';
 import Header from './components/Header';
 // import Footer from './components/Footer';
 import CommentPost from './components/CommentPost';
-import HomePage from './components/HomePage';
+import HomePageLeft from './components/HomePageLeft';
+import HomePageRight from './components/HomePageRight';
 import Sort from './components/Sort';
 import moment from "moment";
 
@@ -62,7 +63,8 @@ function header() {
 
 function StartApp(){
 
-    appDivLeft.innerHTML = HomePage();
+    appDivLeft.innerHTML = HomePageLeft();
+    appDivRight.innerHTML = HomePageRight();
 }
 
 appDivLeft.addEventListener('click', function () {
