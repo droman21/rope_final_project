c26279753d46a403b02d0eb584d238e7084072c0
Daniel Roman <dan.roman21@gmail.com>
2020-08-11 13:18:27 -0400

    WIP table background color update

--- frontend-spa/src/js/components/HomePage.js
+++ frontend-spa/src/js/components/HomePage.js
@@ -1,6 +1,5 @@
 export default function HomePage() {
     return `
-    <h1>R.O.P.E. Application</h1>
     <h2>Your Release: Easily Managed</h2>
     <br>
         <button class="startapp"><b>Click Here to Begin</b></a></button> 
