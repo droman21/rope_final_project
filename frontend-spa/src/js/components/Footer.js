1a30635b67c4e7c4f760c8381c0d2b3a24df91e1
Daniel Roman <dan.roman21@gmail.com>
2020-08-12 13:14:52 -0400

    WIP footer styling

--- frontend-spa/src/js/components/Footer.js
+++ frontend-spa/src/js/components/Footer.js
@@ -1,8 +1,7 @@
 export default function Footer(_) {
     return `
-    <footer class="footer" 
+    <footer class="footer">
     <small>&copy; Copyright 2020, ROPE Inc. All Rights Reserved</small> 
-    <button class="Contact Us"><b>Contact Us</button>
     </footer>
     `
 }
