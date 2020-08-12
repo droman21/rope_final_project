1f4c29d55618f0f6c6e562c88491999cb9ddf2e9
Bernard828 <bernard.lackey1@gmail.com>
2020-08-12 14:56:53 -0400

    added flashing background, change images

--- frontend-spa/src/js/components/ReleaseTask.js
+++ frontend-spa/src/js/components/ReleaseTask.js
@@ -6,43 +6,47 @@ export default function ReleaseTask(releaseTask) {
 
     switch (releaseTask.priority.value) {
         case 1:
-            imageSrc = "images/critical.png"
+            imageSrc = "images/chainHigh.png"
+                //"images/critical.png"
             break;
         case 2:
-            imageSrc = "images/high.png"
+            imageSrc = "images/chainHigh.png"
+            //"images/high.png"
             break;
         case 3:
-            imageSrc = "images/medium.png"
+            imageSrc = "images/chainHigh.png"
+            //"images/medium.png"
             break;
         case 4:
-            imageSrc = "images/low.png"
+            imageSrc = "images/chainLow.png"
+            //"images/low.png"
             break;
         default:
             imageSrc = "images/rope.png"
     }
     return `
     <div class="IconHeading">
-        <img src="${imageSrc}" width="100" height="100">
+        <img class="priorityColor" id="${releaseTask.currentPriorityID}"src="${imageSrc}" width="100" height="100">
         <h2 class="table-2__header"  id="${releaseTask.id}">${releaseTask.id}: ${releaseTask.name}</h2>
     </div>
     <section class="releaseTask__detailsInfo">
-    <button class="edit__releaseTaskButton" id="${releaseTask.id}">Edit</button>
-    <button class="add__commentButton" id="${releaseTask.id}">Add Comment</button>
-    <button class="delete_releaseTaskButton" id="${releaseTask.id}">Delete</button>
+            <button class="edit__releaseTaskButton" id="${releaseTask.id}">Edit</button>
+            <button class="add__commentButton" id="${releaseTask.id}">Add Comment</button>
+            <button class="delete_releaseTaskButton" id="${releaseTask.id}">Delete</button>
         <div class="releaseTask__detailsInfolist">
-            <p class="releaseTask__currentPriorityName"><b>Priority:</b> ${releaseTask.priority.name}</p>
+            <p class="releaseTask__currentPriorityName"><b>Priority:</b><span class="prioritytest" id="${releaseTask.currentPriorityID}"> ${releaseTask.priority.name}</span></p>
             <p class="releaseTask__currentDueTime"><b>Due:</b> ${moment(releaseTask.currentDueTime).format(('MMMM Do YYYY, h:mm a'))}</p>
             <p class="releaseTask__currentStatusName"><b>Status:</b> ${releaseTask.status.name}</p>   
             <p class="releaseTask__assignedEmployeeName"><b>Assigned To:</b> ${releaseTask.employee.name}</p>
             <p class="releaseTask__description"><b>Description:</b> ${releaseTask.description}</p>
             <p class="releaseTask__lastModifiedDate"><b>Last Modified:</b> ${moment(releaseTask.lastModifiedDate).format(('MMMM Do YYYY, h:mm a'))}</p>
-        <ul class="releaseTask__comment"><b>Comments:</b>
+            <p class="releaseTask__comment"><b>Comments:</b>
                 ${releaseTask.comments.map(comment => {
                     return `
                     <li type="date">${comment.details}</li>
                     `
                 }).join("")}
-        </ul>
+            </p>
             <p class="releaseTask__createdDate"><b>Created:</b> ${moment(releaseTask.createdDate).format(('MMMM Do YYYY, h:mm a'))}</p>
         </div>
     </section>
